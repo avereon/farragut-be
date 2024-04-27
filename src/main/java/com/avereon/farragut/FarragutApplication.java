0cf17c9c6679c6f2edd0f19a6da1c3ebaee3eae4
Mark Soderquist <mark@soderquist.net>
2024-04-27 12:39:59 -0600

    Initial setup

---
+++ src/main/java/com/avereon/farragut/FarragutApplication.java
@@ -0,0 +1,13 @@
+package com.avereon.farragut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarragutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarragutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avereon/farragut/FarragutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avereon.farragut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarragutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
