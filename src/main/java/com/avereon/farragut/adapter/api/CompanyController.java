53165f552417e289e284c8ffa235a25c2d8583a5
msoderquist <msoderquist@mastercontrol.com>
2024-05-13 15:48:36 -0600

    Add api generation

--- src/main/java/com/avereon/farragut/adapter/api/CompanyController.java
+++ src/main/java/com/avereon/farragut/adapter/api/CompanyController.java
@@ -1,12 +1,25 @@
 package com.avereon.farragut.adapter.api;
 
-import org.openapitools.api.CompanyApi;
-import org.openapitools.model.Company;
+import com.avereon.farragut.adapter.api.model.Company;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CompanyController implements CompanyApi {
+//    @Override
+//    public ResponseEntity<List<Company>> getCompanies() {
+//        return ResponseEntity.ofNullable(List.of());
+//    }
+
+    @Override
+    public ResponseEntity<List<Company>> getCompanies(Pageable pageable) {
+        return null;
+    }
+
     @Override
     public ResponseEntity<Company> getCompany(String id) {
         return null;
