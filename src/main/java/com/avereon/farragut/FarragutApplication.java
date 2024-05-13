d02ee97eff86a49fdaff613e70bc3acb58e840a1
msoderquist <msoderquist@mastercontrol.com>
2024-05-13 15:50:02 -0600

    Add api generation

--- src/main/java/com/avereon/farragut/FarragutApplication.java
+++ src/main/java/com/avereon/farragut/FarragutApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarragutApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FarragutApplication.class, args);
+	public static void main( String[] args ) {
+		SpringApplication.run( FarragutApplication.class, args );
 	}
 
 }
--- src/main/java/com/avereon/farragut/adapter/api/CompanyController.java
+++ src/main/java/com/avereon/farragut/adapter/api/CompanyController.java
@@ -1,7 +1,6 @@
 package com.avereon.farragut.adapter.api;
 
 import com.avereon.farragut.adapter.api.model.Company;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +9,15 @@ import java.util.List;
 
 @RestController
 public class CompanyController implements CompanyApi {
-//    @Override
-//    public ResponseEntity<List<Company>> getCompanies() {
-//        return ResponseEntity.ofNullable(List.of());
-//    }
 
-    @Override
-    public ResponseEntity<List<Company>> getCompanies(Pageable pageable) {
-        return null;
-    }
+	@Override
+	public ResponseEntity<List<Company>> getCompanies( Pageable pageable ) {
+		return null;
+	}
 
-    @Override
-    public ResponseEntity<Company> getCompany(String id) {
-        return null;
-    }
+	@Override
+	public ResponseEntity<Company> getCompany( String id ) {
+		return null;
+	}
+
 }
