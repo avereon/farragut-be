e52260403a4fe6818dd18d3d4bbd9f4af441dacf
msoderquist <msoderquist@mastercontrol.com>
2024-05-16 13:17:56 -0600

    Move integration tests to failsafe

--- src/test/java/com/avereon/farragut/FarragutApplicationIT.java
+++ src/test/java/com/avereon/farragut/FarragutApplicationIT.java
@@ -3,8 +3,9 @@ package com.avereon.farragut;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// FIXME This is a service test and should not be run as part of the normal build
 @SpringBootTest
-class FarragutApplicationTests {
+class FarragutApplicationIT {
 
 	@Test
 	void contextLoads() {
