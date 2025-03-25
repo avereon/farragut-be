6f8a34e0105576f8be5c251c0825c75e29874235
msoderquist <msoderquist@mastercontrol.com>
2025-03-25 17:34:52 -0600

    Implement JWT authentication: add JwtToken and JwtTokenProvider classes, configure JWT settings, and define user roles

--- src/test/java/com/avereon/farragut/adapter/web/jwt/JwtTokenTest.java
+++ src/test/java/com/avereon/farragut/adapter/web/jwt/JwtTokenTest.java
@@ -1,3 +1,49 @@
 package com.avereon.farragut.adapter.web.jwt;
 
-public class JwtTokenTest {}
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class JwtTokenTest {
+
+	@Test
+	void getToken() {
+		// given
+
+		// when
+		JwtToken token = new JwtToken( "token" );
+
+		// then
+		assertThat( token.getToken() ).isEqualTo( "token" );
+	}
+
+	@Test
+	void setToken() {
+		// given
+		JwtToken token = new JwtToken( "token" );
+
+		// when
+		token.setToken( "newToken" );
+
+		// then
+		assertThat( token.getToken() ).isEqualTo( "newToken" );
+	}
+
+	@Test
+	void extractToken() {
+		// given
+		String bearerToken = JwtToken.AUTHORIZATION_TYPE + " token";
+		HttpServletRequest request = mock( HttpServletRequest.class );
+		when( request.getHeader( JwtToken.AUTHORIZATION_HEADER ) ).thenReturn( bearerToken );
+
+		// when
+		String token = JwtToken.extractToken( request );
+
+		// then
+		assertThat( token ).isEqualTo( "token" );
+	}
+
+}
